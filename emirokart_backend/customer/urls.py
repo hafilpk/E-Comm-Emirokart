44faf9065bdc2cc63deb458356ca665542ecf10b
hafilpk <pkmshafil@gmail.com>
2025-08-14 18:01:36 +0530

    create signupapi, public  and protected APIs

--- emirokart_backend/customer/urls.py
+++ emirokart_backend/customer/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .controller import AuthController
+from .controllers import auth_controller
 
 urlpatterns = [
-    path('login/',AuthController.LoginAPIView.as_view(),name='login'),
+    path('signup/', auth_controller.SignupAPIView.as_view(), name='signup'),
+    path('login/',auth_controller.LoginAPIView.as_view(),name='login'),
+    path('publicApi/',auth_controller.PublicAPIView.as_view(),name='publicapi'),
+    path('protectedApi/',auth_controller.ProtectedAPIView.as_view(),name='protectedapi'),
 ]
